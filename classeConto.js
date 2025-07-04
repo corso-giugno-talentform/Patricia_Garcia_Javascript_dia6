1902e5ff044542b01f0755c5b279978c6a47e87f
Jeanny Patricia Garcia <patgarcia.abafg@gmail.com>
2025-07-04 14:45:32 +0200

    xx

--- classeConto.js
+++ classeConto.js
@@ -5,6 +5,8 @@ function ContoBancario (numeroconto, intestatario, saldo){
     this.numeroconto = numeroconto;
     this.intestatario = intestatario;
     this.saldo = saldo;
+    this.interessi = 1/100;
+    this.annoInteressi = 1;
 
     this.visualizzaSaldo = function(saldo){
         console.log(`il tuo nuovo saldo è: ${this.saldo}`);
@@ -16,7 +18,7 @@ function ContoBancario (numeroconto, intestatario, saldo){
         }
     this.prelevaValore = function(){
         let valore = parseInt(prompt('Quanto vuoi retirare'));
-        if(saldo > valore){
+        if(this.saldo > valore){
                 saldo =  saldo-valore;
                 this.saldo = saldo;
                 console.log(`il tuo nuovo saldo è ${saldo}`);
@@ -26,8 +28,8 @@ function ContoBancario (numeroconto, intestatario, saldo){
         }
 
     this.calcolaInteressi = function(){
-        let interessi = this.saldo * 0.01;
-        console.log(`Gli interessi di ${this.saldo} al 1% annuo è ${interessi} `)
+        let interessi1 = this.saldo * (this.interessi * this.annoInteressi);
+        console.log(`Gli interessi di ${this.saldo} al 1% annuo è ${interessi1} `)
         }
 
 
@@ -39,7 +41,8 @@ let conto2 = new ContoBancario ( 987654321, 'Laura Bianchi', 2350);
 let conto3 = new ContoBancario ( 456789123, 'Mario Rossi', 875.00);
 
 
-conto1.visualizzaSaldo();
-conto1.depositaValore();
-conto1.prelevaValore();
-conto1.calcolaInteressi();
+conto2.visualizzaSaldo();
+conto2.depositaValore();
+conto2.prelevaValore();
+conto2.calcolaInteressi();
+
